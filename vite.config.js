8d0450c96894e106597e82f81a8500c9d6c967df
Syed shazama <syedshazama123@gmail.com>
2025-01-06 17:35:40 +0530

    roolup update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-pm",
-    project: "javascript-react"
-  })],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-pm",
+      project: "javascript-react",
+    }),
+    visualizer({ open: true }), // Generates stats.html and opens it automatically
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
